33055ea3fc8217c9981b8a4b769034ae02b93738
maha faiz <mahafayez02@gmail.com>
2026-02-04 21:01:33 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function appear(){
+    var x=document.getElementById("mega-menu");
+
+    if (x.style.display==="none"){
+        x.style.display="flex";
+    }
+    else{
+        x.style.display="none";  
+    }
+}
